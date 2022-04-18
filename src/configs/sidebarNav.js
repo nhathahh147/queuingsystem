a64fe59669249d52d4887a90ecf3171e2d9dcd42
nhathahh147 <nhathahh147@gmai.com>
2022-04-18 22:07:11 +0700

    first commit

---
+++ src/configs/sidebarNav.js
@@ -0,0 +1,40 @@
+const sidebarNav = [
+    {
+        link: '/',
+        section: 'home',
+        icon: <i class='bx bxs-dashboard'></i>,
+        text: 'Dashboard'
+    },
+    {
+        link: '/orders',
+        section: 'orders',
+        icon: <i class='bx bx-desktop' ></i>,
+        text: 'Thiết bị'
+    },
+    {
+        link: '/products',
+        section: 'products',
+        icon: <i class='bx bx-conversation' ></i>,
+        text: 'Dịch vụ'
+    },
+    {
+        link: '/customers',
+        section: 'customers',
+        icon: <i class='bx bxs-coin-stack' ></i>,
+        text: 'Cấp số'
+    },
+    {
+        link: '/stats',
+        section: 'stats',
+        icon: <i class='bx bxs-report' ></i>,
+        text: 'Báo cáo'
+    },
+    {
+        link: '/settings',
+        section: 'settings',
+        icon: <i className='bx bx-cog'></i>,
+        text: 'Cài đặt hệ thống'
+    }
+]
+
+export default sidebarNav
